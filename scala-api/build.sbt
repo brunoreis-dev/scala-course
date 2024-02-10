32d58b4e1988cca9ce381db79a1f85ef8cc7dbad
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-10 18:28:50 -0300

    implements routes, akka spray json lib and solve some bugs

--- scala-api/build.sbt
+++ scala-api/build.sbt
@@ -8,7 +8,7 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-http" % "10.5.0",
       "com.typesafe.akka" %% "akka-actor-typed" % "2.8.0",
-      "io.spray" %%  "spray-json" % "1.3.6",
+      "com.typesafe.akka" %% "akka-http-spray-json" % "10.5.0",
       "com.typesafe.akka" %% "akka-stream" % "2.8.0"
     )
   )
--- scala-api/src/main/scala/resource/ProductServer.scala
+++ scala-api/src/main/scala/resource/ProductServer.scala
@@ -1,14 +1,17 @@
 package resource
 
 import akka.actor.ActorSystem
+import model.Product
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import service.ProductService
-import model.Product
 import spray.json.DefaultJsonProtocol._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util._
 
-class ProductServer extends App {
+object ProductServer extends App {
   implicit val system = ActorSystem("ProductServer")
 
   val productService = new ProductService()
@@ -20,18 +23,20 @@ class ProductServer extends App {
       complete("Success!")
     }
   } ~ path("products" / "test") {
-    complete("Verified!")
+    get {
+      complete("Verified!")
+    }
   } ~ path("products") {
     get {
       onComplete(productService.getAllProducts()) {
-        case util.Success(res) =>  complete(res)
-        case util.Failure(exception) => complete(StatusCodes.InternalServerError)
+        case Success(res) =>  complete(res)
+        case Failure(exception) => complete(StatusCodes.InternalServerError)
       }
     } ~ post {
       entity(as[Product]) {product =>
         onComplete(productService.saveProduct(product)) {
-          case util.Success(res) =>  complete(res)
-          case util.Failure(exception) => complete(StatusCodes.InternalServerError)
+          case Success(res) =>  complete(res)
+          case Failure(exception) => complete(StatusCodes.InternalServerError)
         }
       }
     }
